188a1238ad91544c679dac6632077eaa9b5a34d8
saltcam <saltcam@uw.edu>
2024-05-04 21:40:54 -0700

    Looking good!

--- gameengine.js
+++ gameengine.js
@@ -18,43 +18,53 @@ class GameEngine {
         // Options and the Details
         this.options = options || {
             debugging: false,
-        };
-    };
+        }
+    }
+
+    // The plan: have two agents race for a prize. The catch is that some tiles can only be passed if the color matches the agent
+    // Agent attempts to move, board returns new xy coords for the agent as well as any points they earned or lost. Dying respawns them.
+    // Agent updates personal internal board with updated compass values to remember which tiles warrant what movement.
+    // if an agent reaches a prize tile, they get reward points and respawn. The other gets punished.
+    // The goal is to see how different strategies develop for agents that are on the same map but have tiles only they can walk on
 
     init(ctx) {
         this.ctx = ctx;
         this.startInput();
         this.timer = new Timer();
-    };
+
+        this.gameBoard = new GameBoard(this);
+
+        // this.addEntity(new agent(this, 0, 0, 0));
+    }
 
     start() {
         this.running = true;
         const gameLoop = () => {
             this.loop();
             requestAnimFrame(gameLoop, this.ctx.canvas);
-        };
+        }
         gameLoop();
-    };
+    }
 
     startInput() {
         const getXandY = e => ({
             x: e.clientX - this.ctx.canvas.getBoundingClientRect().left,
             y: e.clientY - this.ctx.canvas.getBoundingClientRect().top
-        });
+        })
         
         this.ctx.canvas.addEventListener("mousemove", e => {
             if (this.options.debugging) {
                 console.log("MOUSE_MOVE", getXandY(e));
             }
             this.mouse = getXandY(e);
-        });
+        })
 
         this.ctx.canvas.addEventListener("click", e => {
             if (this.options.debugging) {
                 console.log("CLICK", getXandY(e));
             }
             this.click = getXandY(e);
-        });
+        })
 
         this.ctx.canvas.addEventListener("wheel", e => {
             if (this.options.debugging) {
@@ -62,7 +72,7 @@ class GameEngine {
             }
             e.preventDefault(); // Prevent Scrolling
             this.wheel = e;
-        });
+        })
 
         this.ctx.canvas.addEventListener("contextmenu", e => {
             if (this.options.debugging) {
@@ -70,25 +80,27 @@ class GameEngine {
             }
             e.preventDefault(); // Prevent Context Menu
             this.rightclick = getXandY(e);
-        });
+        })
 
         this.ctx.canvas.addEventListener("keydown", event => this.keys[event.key] = true);
         this.ctx.canvas.addEventListener("keyup", event => this.keys[event.key] = false);
-    };
+    }
 
     addEntity(entity) {
         this.entities.push(entity);
-    };
+    }
 
     draw() {
         // Clear the whole canvas with transparent color (rgba(0, 0, 0, 0))
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
+        this.gameBoard.draw(this.ctx);
+
         // Draw latest things first
         for (let i = this.entities.length - 1; i >= 0; i--) {
-            this.entities[i].draw(this.ctx, this);
+            this.entities[i].draw(this.ctx);
         }
-    };
+    }
 
     update() {
         let entitiesCount = this.entities.length;
@@ -106,14 +118,12 @@ class GameEngine {
                 this.entities.splice(i, 1);
             }
         }
-    };
+    }
 
     loop() {
         this.clockTick = this.timer.tick();
         this.update();
         this.draw();
-    };
+    }
 
-};
-
-// KV Le was here :)
+}
--- main.js
+++ main.js
@@ -6,6 +6,14 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
+	ASSET_MANAGER.queueDownload("./assets/Link_Green.png");
+	ASSET_MANAGER.queueDownload("./assets/Link_Blue.png");
+	ASSET_MANAGER.queueDownload("./assets/Zelda.png");
+	ASSET_MANAGER.queueDownload("./assets/Empty.png");
+	ASSET_MANAGER.queueDownload("./assets/Green.png");
+	ASSET_MANAGER.queueDownload("./assets/Blue.png");
+	ASSET_MANAGER.queueDownload("./assets/Die.png");
+
 	gameEngine.init(ctx);
 
 	gameEngine.start();
